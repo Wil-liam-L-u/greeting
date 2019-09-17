acc678a927b85a91a95617f397e1de78fc4fbb16
Wil-liam-L-u <2021blu@dwight.edu>
2019-09-17 13:32:02 +0000

    first

---
+++ authentication.py
@@ -0,0 +1,9 @@
+name = input("what is your name")
+password = input(" what is your password")
+
+if name == "William" and password == "123":
+    print("Hello, William, I'm ur biggest fan")
+elif name == "William":
+    print("that's not right")
+
+
